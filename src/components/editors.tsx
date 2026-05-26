01003f5123a8dfddbe1a83c51aa7714d829bd76b
coderAung <yewontaung798@gmail.com>
2026-05-26 22:37:15 +0630

    feat: Add emmet plugin

--- src/components/editors.tsx
+++ src/components/editors.tsx
@@ -6,6 +6,7 @@ import { useModalControl } from "../hooks/use-controls"
 import { AppModal } from "./app/app-modal"
 import { useCodes } from "../hooks/use-codes"
 import * as Monaco from "monaco-editor"
+import { emmetHTML } from "emmet-monaco-es"
 
 export function EditorSpace() {
     return (
@@ -17,17 +18,19 @@ export function EditorSpace() {
 }
 
 function CodeEditor() {
-    const {setCodes} = useCodes()
-    const save = (value?:string) => {
-        if (! value) return
+    const { setCodes } = useCodes()
+    const save = (value?: string) => {
+        if (!value) return
         setCodes(value)
     }
 
-    const onMount:OnMount = (editor, monaco: typeof Monaco) => {
+    const onMount: OnMount = (editor, monaco: typeof Monaco) => {
         editor.focus()
         editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
             save(editor.getValue())
         })
+
+        emmetHTML(monaco, ["html"])
     }
     return (
         <div className="">
@@ -41,7 +44,7 @@ function CodeEditor() {
                         fontWeight: "bold",
                         fontFamily: "JetBrains Mono",
                         minimap: { enabled: false },
-                        padding: {top: 16, bottom: 16}
+                        padding: { top: 16, bottom: 16 }
                     }
                 } />
         </div>
@@ -49,9 +52,9 @@ function CodeEditor() {
 }
 
 function EditorNav() {
-    const {codes, setFramework} = useCodes()
+    const { codes, setFramework } = useCodes()
     const copy = async () => {
-        if(!codes) return
+        if (!codes) return
         await navigator.clipboard.writeText(codes)
     }
     return (
@@ -75,16 +78,16 @@ function EditorNav() {
 }
 
 const ReactButton = () => {
-    const {isOpen, open, close} = useModalControl()
+    const { isOpen, open, close } = useModalControl()
     return (
         <>
             <button onClick={open} className="bg-blue-500 text-white py-1 px-3">React</button>
-            <ReactModal {...{isOpen, open, close}} />
+            <ReactModal {...{ isOpen, open, close }} />
         </>
-    )    
+    )
 }
 
-const ReactModal = ({isOpen, open, close}: {isOpen:boolean, open:() => void, close:() => void}) => {
+const ReactModal = ({ isOpen, open, close }: { isOpen: boolean, open: () => void, close: () => void }) => {
     return (
         <AppModal isOpen={isOpen} open={open} close={close}>
             <AppModal.Dialog>
@@ -99,10 +102,10 @@ const ReactModal = ({isOpen, open, close}: {isOpen:boolean, open:() => void, clo
     )
 }
 
-const CopyButton = ({copy}: {copy:() => void}) => {
+const CopyButton = ({ copy }: { copy: () => void }) => {
     const [isCopying, setIsCopying] = useState(false)
     const onClick = () => {
-        if(isCopying) return
+        if (isCopying) return
         setIsCopying(true)
         copy()
         setTimeout(() => setIsCopying(false), 2000)
